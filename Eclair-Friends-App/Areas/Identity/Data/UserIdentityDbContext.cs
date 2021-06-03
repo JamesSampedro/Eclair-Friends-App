2075feb62b29c9d6e459872aee4d56dc1f727771
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2021-06-03 21:00:50 +0800

    remake

---
+++ Eclair-Friends-App/Areas/Identity/Data/UserIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FriendsApp.Areas.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendsApp.Areas.Identity.Data
+{
+    public class UserIdentityDbContext : IdentityDbContext
+    {
+        public UserIdentityDbContext(DbContextOptions<UserIdentityDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ Eclair-Friends-App/Areas/Identity/Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace FriendsApp.Areas.Identity.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+    }
+}
